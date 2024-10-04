ef74ba8aedf50badc5cb0502d74f81ddea0ec592
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-04 19:22:37 -0300

    🌙🌙🌙

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Kuroda\nLady K', {
+say('Simple\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
@@ -82,4 +82,4 @@ async function start(files) {
   }
 }
 
-start(['starlights.js'])
+start(['start.js'])
