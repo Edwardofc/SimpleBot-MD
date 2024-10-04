a6fc5afb72333742c32521a18f26c7b295a88499
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-04 19:32:02 -0300

    MCT

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Ian;;\nFN:Ian⁩\nORG:Ian⁩\nTITLE:\nitem1.TEL;waid=5493405480284:5493405480284\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:Solo consultas sobre la bot\nX-WA-BIZ-NAME:Ian\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Ian⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Edward;;\nFN:Ian⁩\nORG:Edward⁩\nTITLE:\nitem1.TEL;waid=51997384622:51997384622\nitem1.X-ABLabel:\nX-WA-BIZ-DESCRIPTION:Creador Oficial\nX-WA-BIZ-NAME:Edward\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Edward⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
